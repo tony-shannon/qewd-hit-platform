b3e60b1ce9a234ad595681f802baa4ef5a9d2e93
Tony Shannon <tony.shannon@ripple.foundation>
2019-09-05 14:42:55 +0100

    update openEHR MS docker run with HITtemplate map

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ docker run -it --name oidc_provider --rm --network qewd-hit -p 8081:8080 -v ~/qe
 docker run -it --name oidc-client --rm --network qewd-hit -v ~/qewd-hit-platform/oidc-client:/opt/qewd/mapped -e mode="microservice" rtweed/qewd-server
 docker run -it --name mpi_service --rm --network qewd-hit -v ~/qewd-hit-platform/fhir-mpi:/opt/qewd/mapped -e mode="microservice" rtweed/qewd-server
 docker run -it --name openehr_service --rm --network qewd-hit -v ~/qewd-hit-platform/openehr-ms:/opt/qewd/mapped -e mode="microservice" rtweed/qewd-server
+OR
+docker run -it --name openehr_service --rm --network qewd-hit -v ~/qewd-hit-platform/openehr-ms:/opt/qewd/mapped -v ~/hit-templates/clinical-headings-templates:/opt/qewd/mapped/templates  -e mode="microservice" rtweed/qewd-server
 docker run -it --name audit_service --rm --network qewd-hit -v ~/qewd-hit-platform/audit-ms:/opt/qewd/mapped -e mode="microservice" rtweed/qewd-server
 docker run -it --rm --name ethercis-db --net ecis-net -p 5432:5432 rtweed/ethercis-db
 docker run -it --rm --name ethercis-server --net ecis-net -e DB_IP=ethercis-db -e DB_PORT=5432 -e DB_USER=postgres -e DB_PW=postgres -p 8082:8080 rtweed/ethercis-server
