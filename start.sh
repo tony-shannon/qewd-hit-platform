4a1922d72f3b6400a5f5e90e20d24b83d2fb6516
Tony Shannon <tony.shannon@ripple.foundation>
2019-09-06 16:48:30 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,7 @@ OR
 docker run -it --name openehr_service --rm --network qewd-hit -v ~/qewd-hit-platform/openehr-ms:/opt/qewd/mapped -v ~/hit-templates/clinical-headings-templates:/opt/qewd/mapped/templates  -e mode="microservice" rtweed/qewd-server
 docker run -it --name audit_service --rm --network qewd-hit -v ~/qewd-hit-platform/audit-ms:/opt/qewd/mapped -e mode="microservice" rtweed/qewd-server
 docker run -it --rm --name ethercis-db --net ecis-net -p 5432:5432 rtweed/ethercis-db
+OR
+docker run -it --rm --name ethercis-db --net ecis-net -p 5432:5432 -v ~/backup-stuff/ecis:/var/lib/postgresql/pgdata rtweed/ethercis-db 
 docker run -it --rm --name ethercis-server --net ecis-net -e DB_IP=ethercis-db -e DB_PORT=5432 -e DB_USER=postgres -e DB_PW=postgres -p 8082:8080 rtweed/ethercis-server
 
